7b981cf4f24050650e5e541ddc59c75dd2ed1cb0
Travis Reynolds <travis@cirruscreative.co.uk>
2020-09-11 22:18:56 +0000

    Update rules to ignore unused vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
-  extends: ['travisreynolds-node-ts']
+  extends: ['travisreynolds-node-ts'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: '_' }],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '_' }]
+  }
 }
