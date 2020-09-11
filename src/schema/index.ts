110ec771c9a6249587349def3d4dbf627010f496
Travis Reynolds <travis@cirruscreative.co.uk>
2020-09-11 23:50:55 +0000

    Udpate to return schema, and remove try/catch

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,8 +1,7 @@
-import { GraphQLAbstractType } from 'graphql'
+import { GraphQLAbstractType, GraphQLSchema } from 'graphql'
 import { createSchemaTypes } from './create-types'
 import { fetchSchema } from './fetch-schema'
 import { renameType } from 'graphql-tools'
-import { reporter } from '../utils'
 import { transformSchema } from './transform-schema'
 
 export interface SchemaUtils {
@@ -11,28 +10,22 @@ export interface SchemaUtils {
   prefix: Function
 }
 
-export const createSchema = async (actions: any, utils: SchemaUtils) => {
+export const createSchema = async (actions: any, utils: SchemaUtils): Promise<GraphQLSchema> => {
   const { baseUrl, prefix } = utils
 
-  try {
-    const { schema, data } = await fetchSchema(baseUrl)
-    const transformedSchema = transformSchema(schema, utils)
+  const { schema, data } = await fetchSchema(baseUrl)
+  const transformedSchema = transformSchema(schema, utils)
 
-    const schemaTypes = await createSchemaTypes(transformedSchema, data, actions, utils)
-    actions.addSchemaTypes(schemaTypes)
+  const schemaTypes = await createSchemaTypes(transformedSchema, data, actions, utils)
+  actions.addSchemaTypes(schemaTypes)
 
-    const nodeType = schema.getType('Node') as GraphQLAbstractType
-    const possibleTypes = schema.getPossibleTypes(nodeType)
-    const collections = possibleTypes.map(type => renameType(type, prefix(type.name))).map(type => type.name)
+  const nodeType = schema.getType('Node') as GraphQLAbstractType
+  const possibleTypes = schema.getPossibleTypes(nodeType)
+  const collections = possibleTypes.map(type => renameType(type, prefix(type.name))).map(type => type.name)
 
-    for (const type of collections) {
-      actions.addCollection(type)
-    }
-
-    return
-  } catch (error) {
-    // ! Will probably need to handle this better in future
-    reporter.error(error.message)
-    return []
+  for (const type of collections) {
+    actions.addCollection(type)
   }
+
+  return transformedSchema
 }
