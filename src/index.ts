26fa61fcb131e7d430e218ff05376c5edde2ccfd
Travis Reynolds <travis@cirruscreative.co.uk>
2020-09-12 11:57:03 +0000

    Create demo query, and fetch data

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 // Packages
+import { getNamedType, GraphQLAbstractType, GraphQLFieldMap, isInterfaceType, isObjectType, isScalarType } from 'graphql'
 import { createSchema } from './schema'
 import { reporter } from './utils'
-// import { graphqlQueryBuilder as queryBuilder } from '@wheelroom/graphql-query-builder'
+import { Fields, graphqlQueryBuilder as queryBuilder } from '@wheelroom/graphql-query-builder'
+import got from 'got'
 
 interface SourceOptions {
   typeName: string
@@ -22,7 +24,104 @@ const GridsomeSourceWordPress = (api: any, config: SourceOptions) => {
     const utils = { baseUrl, typeName, prefix }
 
     // Create Schema
-    await createSchema(actions, utils)
+    try {
+      const schema = await createSchema(actions, utils)
+
+      const nodeType = schema.getType('Node') as GraphQLAbstractType
+      const possibleTypes = schema.getPossibleTypes(nodeType)
+
+      const post = possibleTypes[9]
+      const postFields = post.getFields()
+
+      const transformFields = (fields: GraphQLFieldMap<any, any>): Fields =>
+        Object.fromEntries(
+          Object.values(fields).map(field => {
+            const namedType = getNamedType(field.type)
+            if (isScalarType(namedType)) return [field.name, {}]
+            if (isObjectType(namedType)) {
+              if (namedType.name.includes('ConnectionEdge')) {
+                return [
+                  field.name,
+                  {
+                    fields: {
+                      node: {
+                        fields: {
+                          id: {},
+                          __typename: {
+                            alias: 'typeName'
+                          }
+                        }
+                      }
+                    }
+                  }
+                ]
+              }
+              if (namedType.name.includes('Connection')) {
+                return [
+                  field.name,
+                  {
+                    fields: {
+                      nodes: {
+                        fields: {
+                          id: {},
+                          __typename: {
+                            alias: 'typeName'
+                          }
+                        }
+                      }
+                    }
+                  }
+                ]
+              }
+              return [
+                field.name,
+                {
+                  fields: {
+                    some: {}
+                  }
+                }
+              ]
+            }
+
+            if (isInterfaceType(namedType)) {
+              const subFields = namedType.getFields()
+              return [field.name, { fields: transformFields(subFields) }]
+            }
+            return [
+              field.name,
+              {
+                id: {},
+                __typename: {
+                  alias: 'typeName'
+                }
+              }
+            ]
+          })
+        )
+
+      const query = queryBuilder({
+        operationName: 'Posts',
+        operationType: 'query',
+        fields: {
+          posts: {
+            fields: {
+              nodes: {
+                fields: transformFields(postFields)
+              }
+            }
+          }
+        }
+      })
+
+      const { data } = await got.post(baseUrl, {
+        json: { query },
+        resolveBodyOnly: true,
+        responseType: 'json'
+      })
+      console.log(data.posts.nodes[0].author)
+    } catch (error) {
+      reporter.error(error.message)
+    }
 
     if (log) reporter.success('Finished adding WordPress schema')
   })
