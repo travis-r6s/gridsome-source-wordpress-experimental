703f40e81ff0039830f8b42f601f1861b1b6cbb3
Travis Reynolds <travis@cirruscreative.co.uk>
2020-09-27 17:56:02 +0000

    Update to add data fetching & transforming functions

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,55 +1,227 @@
-// import got from 'got'
-// import pMap from 'p-map'
-import { getNamedType, GraphQLNamedType, GraphQLSchema } from 'graphql'
+import got from 'got'
+import pMap from 'p-map'
+import { Field, graphqlQueryBuilder } from '@wheelroom/graphql-query-builder'
+import { getNamedType, GraphQLObjectType, GraphQLSchema, isInterfaceType, isNonNullType, isObjectType, isScalarType, isTypeSubTypeOf } from 'graphql'
+// import { excludedTypes } from '../schema/type-builder'
 import { Utils } from '../utils'
-// import { Utils } from '../utils'
 // import { cleanNodes } from './clean-nodes'
 // import { createQuery } from './create-query'
-import { transformFields } from './transform-fields'
 
-export const fetchData = async (schema: GraphQLSchema, utils: Utils) => {
-  const { prefix } = utils
-  const queryType = schema.getQueryType()
-  // const typeMap = schema.getTypeMap()
+interface FieldTransform {
+  name: string
+  type: string
+  fields: [string, Field]
+  path: string
+}
+interface FieldTransformParent {
+  name: string
+  type: string
+  fields: Map<string, FieldTransform>
+  path: string
+  query: string
+}
 
-  if (!queryType) throw new Error('No root query type found.')
+const excludedFields = [
+  'viewer',
+  'visibleProducts',
+  'users',
+  'userRoles',
+  'themes',
+  'registeredScripts',
+  'registeredStylesheets',
+  'revisions',
+  'orders',
+  'cart',
+  'plugins',
+  'paymentGateways',
+  'contentNodes',
+  'comments',
+  'allSettings',
+  'readingSettings',
+  'discussionSettings',
+  'generalSettings',
+  'writingSettings',
+  'refunds',
+  'coupons',
+  'contentTypes',
+  'contentNodes'
+]
+const excludedTypes = ['EnqueuedScript', 'EnqueuedStylesheet', 'DownloadableItem']
 
-  const queryFields = Object.values(queryType.getFields()).filter(({ type }) => type.toString().includes('Connection'))
+export const fetchData = async (schema: GraphQLSchema, actions: any, utils: Utils) => {
+  const { prefix, baseUrl, concurrency } = utils
 
-  for (const [i, field] of queryFields.entries()) {
-    console.log(i)
-    const type = schema.getType(prefix(field.type.toString())) as GraphQLNamedType
-    const config = type.toConfig() as any
-    const nodesType = config.fields.nodes.type
+  const queryFields = schema.getQueryType()?.getFields()
+  if (!queryFields) return
 
-    const objectType = getNamedType(nodesType) as any
-    const { fields } = schema.getType(objectType)?.toConfig() as any
-    console.log(transformFields(fields))
+  const isConnectionField = (type: GraphQLObjectType) => Object.values(type.getFields()).find(({ name }) => name === 'nodes')
+  const isConnectionEdgeField = (type: GraphQLObjectType) => Object.values(type.getFields()).find(({ name }) => name === 'node')
+
+  const fieldTransformer = (type: GraphQLObjectType): FieldTransform[] =>
+    Object.values(type.getFields())
+      .map(field => {
+        if (excludedFields.includes(field.name)) return
+        const namedType = getNamedType(field.type) as GraphQLObjectType
+
+        if (isScalarType(namedType)) {
+          return {
+            name: field.name,
+            type: field.type.toString(),
+            fields: [field.name, {}],
+            path: ''
+          }
+        }
+
+        if (isObjectType(namedType)) {
+          const nodesField = isConnectionField(namedType)
+          if (nodesField) {
+            const subType = getNamedType(nodesField.type)
+            if (excludedTypes.includes(subType.toString())) return
+            return {
+              name: field.name,
+              type: subType.toString(),
+              fields: [field.name, { fields: { nodes: { fields: { id: {}, __typename: { alias: 'typeName' } } } } }],
+              path: 'nodes'
+            }
+          }
+
+          const nodeField = isConnectionEdgeField(namedType)
+          if (nodeField) {
+            const subType = getNamedType(nodeField.type)
+            if (excludedTypes.includes(subType.toString())) return
+            return {
+              name: field.name,
+              type: subType.toString(),
+              fields: [field.name, { fields: { node: { fields: { id: {}, __typename: { alias: 'typeName' } } } } }],
+              path: 'node'
+            }
+          }
+        }
+
+        if (isInterfaceType(namedType)) {
+          if (isTypeSubTypeOf(schema, namedType, type)) {
+            return {
+              name: field.name,
+              type: namedType.toString(),
+              fields: [field.name, { fields: { id: {}, __typename: { alias: 'typeName' } } }],
+              path: ''
+            }
+          }
+          const fields = fieldTransformer(namedType).map(({ fields }) => fields)
+          return {
+            name: field.name,
+            type: namedType.toString(),
+            fields: [field.name, { fields: Object.fromEntries(fields) }],
+            path: ''
+          }
+        }
+
+        return
+      })
+      .filter(f => !!f) as FieldTransform[]
+
+  const queries = Object.values(queryFields)
+    .map((field): FieldTransformParent | undefined => {
+      // If we have a non null arg, or an ID arg, then we can't use this field
+      const hasNonNullType = field.args.some(({ type }) => isNonNullType(type))
+      const hasIDInput = field.args.find(({ type }) => getNamedType(type).toString() === 'ID')
+      if (hasNonNullType || hasIDInput) return
+      if (excludedFields.includes(field.name)) return
+
+      if (isObjectType(field.type)) {
+        // Check if this is a `nodes` field: if so, we need to get the root type
+        const nodesField = isConnectionField(field.type)
+        if (nodesField) {
+          const type = getNamedType(nodesField.type) as GraphQLObjectType
+          const fields = fieldTransformer(type)
+          const queryFields = Object.fromEntries(fields.map(({ fields }) => fields))
+          const query = graphqlQueryBuilder({ operationType: 'query', fields: { [field.name]: { fields: { nodes: { fields: queryFields } } } } })
+
+          return {
+            name: field.name,
+            type: type.name,
+            fields: new Map(fields.map(field => [field.name, field])),
+            path: 'nodes',
+            query
+          }
+        }
+
+        const fields = fieldTransformer(field.type)
+        const queryFields = Object.fromEntries(fields.map(({ fields }) => fields))
+        const query = graphqlQueryBuilder({ operationType: 'query', fields: { [field.name]: { fields: queryFields } } })
+
+        return {
+          name: field.name,
+          type: field.type.toString(),
+          fields: new Map(fields.map(field => [field.name, field])),
+          path: '',
+          query
+        }
+      }
+
+      console.log(field)
+      return
+    })
+    .filter(f => !!f) as FieldTransformParent[]
+
+  const data = await pMap(
+    queries,
+    async (field: FieldTransformParent) => {
+      const { query, name, path, fields, type } = field
+
+      const { data, errors } = await got.post(baseUrl, {
+        json: { query },
+        resolveBodyOnly: true,
+        responseType: 'json'
+      })
+      if (errors) return console.log(`Errors for ${name}: ${errors[0].message}`)
+
+      const nodes = data[name][path] as any
+      if (!nodes) return console.log(`No nodes found at this path: data.${name}.${path}`)
+
+      const formattedNodes = nodes.map((node: any) => {
+        const nodeFields = Object.entries(node).map(([key, value]: [string, any]) => {
+          const config = fields.get(key)
+          if (!value || !config?.path) return [key, value]
+
+          return [key, value[config.path]]
+        })
+
+        const updatedTypeNames = nodeFields.map(([key, value]) => {
+          if (!value || typeof value !== 'object') return [key, value]
+
+          if (Array.isArray(value)) {
+            const transformedValues = value.map(({ typeName, ...v }) => (typeName ? { ...v, typeName: prefix(typeName) } : v))
+            return [key, transformedValues]
+          }
+          if (value.typeName) {
+            return [key, { ...value, typeName: prefix(value.typeName) }]
+          }
+
+          return [key, value]
+        })
+
+        return Object.fromEntries(updatedTypeNames)
+      })
+
+      return {
+        type: prefix(type),
+        nodes: formattedNodes
+      }
+    },
+    { concurrency }
+  )
+
+  const allData = data.filter(d => !!d) as { type: string; nodes: any[] }[]
+
+  for (const { type, nodes } of allData) {
+    const collection = actions.getCollection(type)
+    if (!collection) {
+      console.log(`No collection for type: ${type}`)
+      continue
+    }
+    for (const node of nodes) {
+      collection.addNode(node)
+    }
   }
-  console.log('done')
-  // const allData = await pMap(
-  //   possibleTypes,
-  //   async type => {
-  //     const fields = type.getFields()
-
-  //     const transformedFields = transformFields(fields)
-
-  //     const query = createQuery(type.name, transformedFields)
-
-  //     const { data, errors } = await got.post(baseUrl, {
-  //       json: { query },
-  //       resolveBodyOnly: true,
-  //       responseType: 'json'
-  //     })
-
-  //     if (errors) throw new Error(errors[0].message)
-
-  //     const cleanedNodes = cleanNodes(data.data, utils)
-
-  //     return cleanedNodes
-  //   },
-  //   { concurrency }
-  // )
-
-  // return allData
 }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
 // Packages
-import { getNamedType, GraphQLAbstractType, GraphQLFieldMap, isInterfaceType, isObjectType, isScalarType } from 'graphql'
 import { createSchema } from './schema'
 import { reporter } from './utils'
-import { Fields, graphqlQueryBuilder as queryBuilder } from '@wheelroom/graphql-query-builder'
-import got from 'got'
+import { fetchData } from './data'
 
 interface SourceOptions {
   typeName: string
   baseUrl: string
   log: boolean
+  concurrency: number
 }
 
 const GridsomeSourceWordPress = (api: any, config: SourceOptions) => {
-  const { typeName = 'WordPress', baseUrl = '', log = false } = config
+  const { typeName = 'WordPress', baseUrl = '', log = false, concurrency = 8 } = config
 
   if (!baseUrl) throw new Error('Missing the `baseUrl` config option.')
   if (!typeName) throw new Error('Missing the `typeName` config option.')
@@ -21,104 +20,13 @@ const GridsomeSourceWordPress = (api: any, config: SourceOptions) => {
     const scalarTypes = ['String', 'Int', 'Float', 'Boolean', 'ID']
     const prefix = (name: string) => (scalarTypes.includes(name) ? name : `${typeName}${name}`)
 
-    const utils = { baseUrl, typeName, prefix }
+    const utils = { baseUrl, typeName, prefix, concurrency }
 
     // Create Schema
     try {
       const schema = await createSchema(actions, utils)
 
-      const nodeType = schema.getType('Node') as GraphQLAbstractType
-      const possibleTypes = schema.getPossibleTypes(nodeType)
-
-      const post = possibleTypes[9]
-      const postFields = post.getFields()
-
-      const transformFields = (fields: GraphQLFieldMap<any, any>): Fields =>
-        Object.fromEntries(
-          Object.values(fields).map(field => {
-            const namedType = getNamedType(field.type)
-            if (isScalarType(namedType)) return [field.name, {}]
-            if (isObjectType(namedType)) {
-              if (namedType.name.includes('ConnectionEdge')) {
-                return [
-                  field.name,
-                  {
-                    fields: {
-                      node: {
-                        fields: {
-                          id: {},
-                          __typename: {
-                            alias: 'typeName'
-                          }
-                        }
-                      }
-                    }
-                  }
-                ]
-              }
-              if (namedType.name.includes('Connection')) {
-                return [
-                  field.name,
-                  {
-                    fields: {
-                      nodes: {
-                        fields: {
-                          id: {},
-                          __typename: {
-                            alias: 'typeName'
-                          }
-                        }
-                      }
-                    }
-                  }
-                ]
-              }
-              return [
-                field.name,
-                {
-                  fields: {
-                    some: {}
-                  }
-                }
-              ]
-            }
-
-            if (isInterfaceType(namedType)) {
-              const subFields = namedType.getFields()
-              return [field.name, { fields: transformFields(subFields) }]
-            }
-            return [
-              field.name,
-              {
-                id: {},
-                __typename: {
-                  alias: 'typeName'
-                }
-              }
-            ]
-          })
-        )
-
-      const query = queryBuilder({
-        operationName: 'Posts',
-        operationType: 'query',
-        fields: {
-          posts: {
-            fields: {
-              nodes: {
-                fields: transformFields(postFields)
-              }
-            }
-          }
-        }
-      })
-
-      const { data } = await got.post(baseUrl, {
-        json: { query },
-        resolveBodyOnly: true,
-        responseType: 'json'
-      })
-      console.log(data.posts.nodes[0].author)
+      await fetchData(schema, actions, utils)
     } catch (error) {
       reporter.error(error.message)
     }
