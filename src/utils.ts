38f0e2252e7884846f6fec73a53ed152bb7a882f
Travis Reynolds <travis@cirruscreative.co.uk>
2020-09-27 13:00:29 +0000

    Add Utils interface

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,10 @@
 import consola from 'consola'
 
 export const reporter = consola.create({ defaults: { tag: 'gridsome-source-wordpress-experimental' } })
+
+export interface Utils {
+  baseUrl: string
+  typeName: string
+  prefix: Function
+  concurrency: number
+}
