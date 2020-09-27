ad73dcc7b025c35fe4f64ef8f401410e5455d752
Travis Reynolds <travis@cirruscreative.co.uk>
2020-09-27 20:57:36 +0000

    Update to handle paged data

--- src/data/fetch-data.ts
+++ src/data/fetch-data.ts
@@ -1,82 +1,80 @@
 import got from 'got/dist/source'
 import pMap from 'p-map'
-import { Utils } from '../utils'
+import { reporter, Utils } from '../utils'
 import { FieldTransformParent } from './transform-fields'
 
 export const fetchData = async (queries: FieldTransformParent[], utils: Utils) => {
   const data = await pMap(
     queries,
     async (field: FieldTransformParent) => {
-      const {
-        query,
-        name,
-        path
-        // fields, type
-      } = field
+      const { query, name, path, fields, type } = field
+
+      let nodes
       if (path === 'nodes') {
-        const data = await got.paginate.all(utils.baseUrl, {
+        nodes = await got.paginate.all(utils.baseUrl, {
           method: 'POST',
           json: { query, variables: { first: utils.perPage } },
           resolveBodyOnly: true,
           responseType: 'json',
           pagination: {
             transform: (response: any) => {
-              console.log(response.body)
-              return response.data[name]
+              const { data, errors } = response.body
+              if (!errors) return data[name].nodes
+
+              reporter.error(`Error when fetching ${name}: ${errors[0].message}`)
+              return []
             },
-            paginate: response => {
-              console.log(response)
-              return false
+            paginate: (response: any, _allItems, currentItems) => {
+              const { hasNextPage, endCursor } = response.body.data[name].pageInfo
+              if (!hasNextPage || !currentItems.length) return false
+              return {
+                json: { query, variables: { first: utils.perPage, after: endCursor } }
+              }
             }
           }
         })
+      } else {
+        const result = (await got.post(utils.baseUrl, {
+          json: { query },
+          resolveBodyOnly: true,
+          responseType: 'json'
+        })) as any
+        if (result.errors) {
+          reporter.error(`Error when getting ${name}: ${result.errors[0].message}`)
+          return { type: '', nodes: [] }
+        }
 
-        console.log(data)
+        nodes = result.data[name][path] as any
       }
 
-      // if (errors) {
-      //   reporter.error(`Error when getting ${name}: ${errors[0].message}`)
-      //   return { type: '', nodes: [] }
-      // }
+      if (!nodes) {
+        reporter.warn(`No nodes found at this path: data.${name}.${path}`)
+        return { type: '', nodes: [] }
+      }
 
-      // const nodes = data[name][path] as any
-      // if (!nodes) {
-      //   reporter.warn(`No nodes found at this path: data.${name}.${path}`)
-      //   return { type: '', nodes: [] }
-      // }
+      const formattedNodes = nodes.map((node: any) => {
+        const nodeFields = Object.entries(node).map(([key, value]: [string, any]) => {
+          const config = fields.get(key)
+          if (!value || !config?.path) return [key, value]
+          return [key, value[config.path]]
+        })
 
-      // const formattedNodes = nodes.map((node: any) => {
-      //   const nodeFields = Object.entries(node).map(([key, value]: [string, any]) => {
-      //     const config = fields.get(key)
-      //     if (!value || !config?.path) return [key, value]
+        const updatedTypeNames = nodeFields.map(([key, value]) => {
+          if (!value || typeof value !== 'object') return [key, value]
+          if (Array.isArray(value)) {
+            const transformedValues = value.map(value => (value.__typename ? { id: value.id, typeName: utils.prefix(value.__typename) } : value))
+            return [key, transformedValues]
+          }
+          if (value.__typename) return [key, { id: value.id, typeName: utils.prefix(value.__typename) }]
+          return [key, value]
+        })
 
-      //     return [key, value[config.path]]
-      //   })
+        return Object.fromEntries(updatedTypeNames)
+      })
 
-      //   const updatedTypeNames = nodeFields.map(([key, value]) => {
-      //     if (!value || typeof value !== 'object') return [key, value]
-
-      //     if (Array.isArray(value)) {
-      //       const transformedValues = value.map(({ typeName, ...v }) => (typeName ? { ...v, typeName: utils.prefix(typeName) } : v))
-      //       return [key, transformedValues]
-      //     }
-      //     if (value.typeName) {
-      //       return [key, { ...value, typeName: utils.prefix(value.typeName) }]
-      //     }
-
-      //     return [key, value]
-      //   })
-
-      //   return Object.fromEntries(updatedTypeNames)
-      // })
-
-      // return {
-      //   type: utils.prefix(type),
-      //   nodes: formattedNodes
-      // }
       return {
-        type: 'Test',
-        nodes: []
+        type: utils.prefix(type),
+        nodes: formattedNodes
       }
     },
     { concurrency: utils.concurrency }
